7d98870fc7df5b7110f48617d8ae96b1cc4a9c29
vigneshwarrr <vigneshwar20012006@outlook.com>
2024-11-03 23:41:10 +0530

    creating project structure (spring boot)

---
+++ src/main/java/com/sqlMate/SqlMateApplication.java
@@ -0,0 +1,17 @@
+package com.sqlMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author VIGNESH
+ *
+ */
+@SpringBootApplication
+public class SqlMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sqlMate/SqlMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqlMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
